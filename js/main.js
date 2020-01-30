5657fe3119b8e3ee07826168d7a6c23cc9c77eff
Oleksii Mykhailov <alem.coding@gmail.com>
2020-01-30 15:33:42 +0200

    Fixed touch

--- js/main.js
+++ js/main.js
@@ -83,11 +83,53 @@ window.addEventListener("DOMContentLoaded", function() {
         ctx.fillStyle = "black";
     }
 
-    function print(e) {
-        if (isMouseDown) {
+    function print(e, touch) {
+        if (isMouseDown || touch) {
             let x = e.clientX,
                 y = e.clientY;
 
+            coords.push([x, y]);
+
+            ctx.lineWidth = lineWidth;
+
+            ctx.lineTo(x, y);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(x, y, lineWidth / 2, 0, Math.PI * 2);
+            ctx.fill();
+
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+        }
+    }
+
+    // touch
+
+    canvas.addEventListener("touchstart", handleStart, false);
+    canvas.addEventListener("touchend", handleEnd, false);
+    canvas.addEventListener("touchcancel", handleCancel, false);
+    canvas.addEventListener("touchmove", handleMove, false);
+
+    function handleStart(e) {
+        console.log("touchstart.");
+    }
+
+    function handleEnd(e) {
+        console.log("touchend.");
+        ctx.beginPath();
+        coords.push("mouseup");
+    }
+    function handleCancel(e) {
+        console.log("touchcancel.");
+    }
+    function handleMove(e) {
+        console.log(e.changedTouches);
+        //coords.push([x, y]);
+        let touches = e.changedTouches;
+        for (let i = 0; i < touches.length; i++) {
+            let x = touches[i].pageX,
+                y = touches[i].pageY;
             coords.push([x, y]);
 
             ctx.lineWidth = lineWidth;
